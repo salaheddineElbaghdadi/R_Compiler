586616e23001dd7ea24e2f851aed6d1365240ec4
mehdi-nait <mehdi.nait.hammou1@gmail.com>
2020-02-15 06:25:19 +0100

    mise à jour de la grammaire et l'analyseur syntaxique

--- syntaxic.c
+++ syntaxic.c
@@ -4,20 +4,268 @@
 #include"global.h"
 #include"lexical.h"
 
+
+bool PROG();
+bool BLOC();
+bool END();
+bool EXPRESSION();
+bool LOOP();
+bool FUNCTION();
+bool DECISION();
+bool OPERATION();
+bool IDENTIF();
+bool OPERATOR();
+bool CALLFUNCTION();
+bool DATA();
+
+
 int main(int argc,char**argv){
 	init();
 	f=fopen("code.R","r");
 	clearBuffer();
 	get_next_char();
-	while(get_next_token()){
-		
-		printf(" %d",token);
-		printf("   %s   \n",buffer);
-		
-		//clearBuffer();
+	get_next_token();
+	if(PROG()){
+		printf("fin!!");
 	}
-
-	printf("  fin   !!\n");
+	else{
+		printf("%d",token);
+		printf("syntax error!");
+	}
+	
+	//printf("  fin   !!\n");
 
 	return 0;
+}
+
+bool PROG(){
+	printf("----> PROG\n");
+	if(BLOC()){
+		while(BLOC());
+		if(END()){
+			return true;
+		}
+		get_next_token();
+	}
+	else if (END()){
+		
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+	
+}
+
+bool BLOC(){
+	printf("---> BLOC\n");
+	if(EXPRESSION()){
+		get_next_token();
+		return true;
+		
+	}
+	else if(LOOP()){
+		get_next_token();
+		return true;
+	}
+	else if(FUNCTION()){
+		get_next_token();
+		return true;
+	}
+	else if (DECISION()){
+		get_next_token();
+		return true;
+	}
+	else if (OPERATION()){
+		get_next_token();
+		return true;
+	}
+	else{
+		return false;
+	}
+
+}
+bool END(){
+	printf("--->END\n");
+	if (token==EOF_TOKEN){
+		//printf("EOF");
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+bool EXPRESSION(){
+	printf("---> EXPRESSION\n");
+	if(token==ID_TOKEN){
+		get_next_token();
+		if(token==LEFT_ASSIGN_TOKEN){
+			get_next_token();
+			if(OPERATION()){
+				get_next_token();
+				return true;
+				
+			}
+			else{
+				return false;
+			}
+		}
+		else{
+			return false;
+		}
+	
+	}
+	else{
+		return false;
+		}
+}
+
+bool LOOP(){
+	return false;
+}
+
+bool FUNCTION(){
+	return false;
+}
+bool DECISION(){
+	return false;
+}
+
+bool IDENTIF(){
+	printf("---> IDENTIF\n");
+	if(token==ID_TOKEN || CALLFUNCTION() || DATA()){
+		get_next_token();
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+bool OPERATION(){
+	printf("---> OPERATION\n");
+	if(IDENTIF()){
+		while(OPERATOR()){
+			if(!IDENTIF()){
+				return false;
+			}
+		}
+		get_next_char();
+		if(is_end_of_line()){
+			get_next_token();
+			return true;
+		}
+		else{
+			return false;
+		}
+		
+	}
+	if(token==OPENING_PARENTHESES_TOKEN){
+		get_next_token();
+		if(OPERATION()){
+				if(token=CLOSING_PARENTHESES_TOKEN){
+					get_next_char();
+					if(is_end_of_line()){
+						get_next_token();
+						return true;
+					}
+				
+	
+				}
+			
+		}
+		else{
+			return false;
+		}
+	}
+}
+
+
+bool OPERATOR(){
+	printf("---> OPERATOR\n");
+	switch(token){
+		case EQUAL_TOKEN:{
+			get_next_token();
+			return true;
+		}
+		case LESS_TOKEN:{
+			get_next_token();
+			return true;
+		}
+		case GREATER_TOKEN:{
+			get_next_token();
+			return true;
+		}
+		case PLUS_TOKEN:{
+			get_next_token();
+			return true;
+		}
+		case SUBSTRACT_TOKEN:{
+			get_next_token();
+			return true;
+		}
+		case MULTIPLY_TOKEN:{
+			get_next_token();
+			return true;
+		}
+		
+		case DEVIDE_TOKEN:{
+			get_next_token();
+			return true;
+		}
+		case LESS_EQUAL_TOKEN:{
+			get_next_token();
+			return true;
+		}
+		case GREATER_EQUAL_TOKEN:{
+			get_next_token();
+			return true;
+		}
+		case NOT_TOKEN:{
+			get_next_token();
+			return true;
+		}
+		case AND_TOKEN:{
+			get_next_token();
+			return true;
+		}
+		case OR_TOKEN:{
+			get_next_token();
+			return true;
+		}
+		case NOT_EQUAL_TOKEN:{
+			get_next_token();
+			return true;
+		}
+		case LOGICAL_OR_TOKEN:{
+			get_next_token();
+			return true;
+		}
+		case LOGICAL_AND_TOKEN:{
+			get_next_token();
+			return true;
+		}
+		case MODULO_TOKEN:{
+			get_next_token();
+			return true;
+		}
+		case QUOTIENT_TOKEN:{
+			get_next_token();
+			return true;
+		}
+		case POWER_TOKEN:{
+			get_next_token();
+			return true;
+		}
+
+	}
+}
+
+bool DATA(){
+	return false;
+}
+
+bool CALLFUNCTION(){
+	return false;
 }
