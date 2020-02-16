5eabe2dd1af2b9cef9ffc4c31561b488e820265f
mehdi-nait <mehdi.nait.hammou1@gmail.com>
2020-02-16 22:50:44 +0100

    Màj de l'analyseur syntaxique

--- syntaxic.c
+++ syntaxic.c
@@ -1,159 +1,201 @@
-
 #include<stdio.h>
 #include<stdlib.h>
+#include"lexical.h"
 #include"global.h"
 #include"lexical.h"
 
-
 bool PROG();
-bool BLOC();
 bool END();
-bool EXPRESSION();
-bool LOOP();
-bool FUNCTION();
+bool BLOC();
 bool DECISION();
 bool OPERATION();
 bool IDENTIF();
-bool OPERATOR();
 bool CALLFUNCTION();
-bool DATA();
+bool OPERATOR();
 
-
-int main(int argc,char**argv){
-	init();
-	f=fopen(argv[1],"r");
+void main(int argc,char**argv){
+	f=fopen("code.R","r");
 	clearBuffer();
 	get_next_char();
 	get_next_token();
 	if(PROG()){
-		printf("fin!!");
+		printf("fin!!\n");
 	}
 	else{
-		printf("syntax error!");
+		printf("syntax error\n");
 	}
-	
-	//printf("  fin   !!\n");
 
-	return 0;
 }
 
 bool PROG(){
+	printf("-->PROG\n");
 	if(BLOC()){
 		while(BLOC());
 		if(END()){
 			return true;
 		}
-		get_next_token();
-	}
-	else if (END()){
-		
-		return true;
-	}
-	else
-	{
-		return false;
-	}
-	
-}
-
-bool BLOC(){
-	if(EXPRESSION()){
-		get_next_token();
-		return true;
-		
-	}
-	else if(LOOP()){
-		get_next_token();
-		return true;
-	}
-	else if(FUNCTION()){
-		get_next_token();
-		return true;
-	}
-	else if (DECISION()){
-		get_next_token();
-		return true;
-	}
-	else{
-		return false;
-	}
-
-}
-bool END(){
-	if (token==EOF_TOKEN){
-		//printf("EOF");
-		return true;
-	}
-}
-bool EXPRESSION(){
-	if(token==ID_TOKEN){
-		get_next_token();
-		if(token==LEFT_ASSIGN_TOKEN){
-			get_next_token();
-			if(OPERATION()){
-				get_next_token();
-				return true;
-				
-			}
-			else{
-				return false;
-			}
-		}
 		else{
 			return false;
 		}
-	
+	}
+	else if(END()){
+		return true;
 	}
 	else{
 		return false;
+	}
+}
+
+bool BLOC(){
+
+	if(OPERATION()){
+		get_next_token();
+		if(token==EOL_TOKEN){
+			get_next_token();
+			while(token==EOL_TOKEN){
+				get_next_token();
+			}
+			return true;
 		}
+	}
+	else{
+		return false;
+	}
 }
 
-bool LOOP(){
-	return false;
+bool END(){
+	if(token==EOF_TOKEN){
+		printf("fin du fichier\n");
+		return true;
+	}
+	else{
+		return false;
+	}
 }
-
-bool FUNCTION(){
-	return false;
-}
-bool DECISION(){
+bool CALLFUNCTION(){
 	return false;
 }
 
 bool IDENTIF(){
-	if(token==ID_TOKEN || CALLFUNCTION() || DATA()){
+	if(token==NUMERIC_TOKEN){
+		get_next_token();
+		return true;
+	}
+	else if(token==ID_TOKEN){
+		get_next_token();
+		return true;
+	}
+	else if(token==CALLFUNCTION()){
 		get_next_token();
 		return true;
 	}
 	else{
 		return false;
+	}
+}
+
+bool OPERATOR(){
+	switch(token){
+		case EQUAL_TOKEN:{get_next_token();return true;}
+		case LESS_TOKEN:{get_next_token();return true;}
+		case LESS_EQUAL_TOKEN:{get_next_token();return true;}
+		case GREATER_TOKEN:{get_next_token();return true;}
+		case GREATER_EQUAL_TOKEN:{get_next_token();return true;}
+		case PLUS_TOKEN:{get_next_token();return true;}
+		case SUBSTRACT_TOKEN:{get_next_token();return true;}
+		case MULTIPLY_TOKEN:{get_next_token();return true;}
+		case DEVIDE_TOKEN:{get_next_token();return true;}
+		case NOT_TOKEN:{get_next_token();return true;}
+		case AND_TOKEN:{get_next_token();return true;}
+		case LOGICAL_AND_TOKEN:{get_next_token();return true;}
+		case OR_TOKEN:{get_next_token();return true;}
+		case LOGICAL_OR_TOKEN:{get_next_token();return true;}
+		case NOT_EQUAL_TOKEN:{get_next_token();return true;}
+		case MODULO_TOKEN:{get_next_token();return true;}
+		case POWER_TOKEN:{get_next_token();return true;}
+		case QUOTIENT_TOKEN:{get_next_token();return true;}
+		default:{
+			return false;
+		}
+		
+
 	}
 }
 
 bool OPERATION(){
 	if(IDENTIF()){
+		get_next_token();
 		while(OPERATOR()){
-			if(!IDENTIF()){
+			if(IDENTIF()){
+				get_next_token();
+				return true;
+			}
+			else{
 				return false;
 			}
 		}
-		get_next_char();
-		if(is_end_of_line()){
-			get_next_token();
-			return true;
-		}
-		else{
-			return false;
-		}
-		
-	}
-	if(token==OPENING_PARENTHESES_TOKEN){
 		get_next_token();
-		if(OPERATION()){
-			while(OPERATION);
-			if(token=CLOSING_PARENTHESES_TOKEN){
-				get_next_token();
-				return true;
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+
+
+
+bool DECISION(){
+	if(token==IF_TOKEN){
+		get_next_token();
+		if(token==OPENING_PARENTHESES_TOKEN){
+			get_next_token();
+			if(OPERATION()){
+				if(token==CLOSING_PARENTHESES_TOKEN){
+					get_next_token();
+					if(token==EOL_TOKEN){
+						get_next_token();
+						if(token==OPENING_CURLY_BRACKETS_TOKEN){
+							get_next_token();
+							if(BLOC()){
+								if(token==CLOSING_CURLY_BRACKETS_TOKEN){
+									get_next_token();
+									printf("function syn correcte\n");
+								}
+								else{
+									return false;
+								}
+							}
+							else{
+								return false;
+							}
+						}
+						else{
+							return false;
+						}
+					}
+					else if(token==OPENING_CURLY_BRACKETS_TOKEN){
+							get_next_token();
+							if(BLOC()){
+								if(token==CLOSING_CURLY_BRACKETS_TOKEN){
+									get_next_token();
+									printf("function syn correcte\n");
+								}
+								else{
+									return false;
+								}
+							}
+							else{
+								return false;
+							}
+			
+						}
+						else{
+							return false;
+						}
+					
+					
+				}
 			}
 			else{
 				return false;
@@ -163,92 +205,7 @@ bool OPERATION(){
 			return false;
 		}
 	}
-}
-
-
-bool OPERATOR(){
-	switch(token){
-		case EQUAL_TOKEN:{
-			get_next_token();
-			return true;
-		}
-		case LESS_TOKEN:{
-			get_next_token();
-			return true;
-		}
-		case GREATER_TOKEN:{
-			get_next_token();
-			return true;
-		}
-		case PLUS_TOKEN:{
-			get_next_token();
-			return true;
-		}
-		case SUBSTRACT_TOKEN:{
-			get_next_token();
-			return true;
-		}
-		case MULTIPLY_TOKEN:{
-			get_next_token();
-			return true;
-		}
-		
-		case DEVIDE_TOKEN:{
-			get_next_token();
-			return true;
-		}
-		case LESS_EQUAL_TOKEN:{
-			get_next_token();
-			return true;
-		}
-		case GREATER_EQUAL_TOKEN:{
-			get_next_token();
-			return true;
-		}
-		case NOT_TOKEN:{
-			get_next_token();
-			return true;
-		}
-		case AND_TOKEN:{
-			get_next_token();
-			return true;
-		}
-		case OR_TOKEN:{
-			get_next_token();
-			return true;
-		}
-		case NOT_EQUAL_TOKEN:{
-			get_next_token();
-			return true;
-		}
-		case LOGICAL_OR_TOKEN:{
-			get_next_token();
-			return true;
-		}
-		case LOGICAL_AND_TOKEN:{
-			get_next_token();
-			return true;
-		}
-		case MODULO_TOKEN:{
-			get_next_token();
-			return true;
-		}
-		case QUOTIENT_TOKEN:{
-			get_next_token();
-			return true;
-		}
-		case POWER_TOKEN:{
-			get_next_token();
-			return true;
-		}
-
+	else{
+		return false;
 	}
-}
-
-bool DATA(){
-	return false;
-}
-
-bool CALLFUNCTION(){
-	return false;
 }
